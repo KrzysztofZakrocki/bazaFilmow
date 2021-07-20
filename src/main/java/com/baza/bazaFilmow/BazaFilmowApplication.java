a8cba80c07b119479fa865e75a57a5e7a604e817
KrzysztofZakrocki <zakrocki.krzysztof@gmail.com>
2021-07-20 20:42:34 +0200

    first commit

---
+++ src/main/java/com/baza/bazaFilmow/BazaFilmowApplication.java
@@ -0,0 +1,13 @@
+package com.baza.bazaFilmow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaFilmowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaFilmowApplication.class, args);
+	}
+
+}
